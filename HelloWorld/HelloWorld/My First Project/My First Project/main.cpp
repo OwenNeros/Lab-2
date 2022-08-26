35c98b4a777a6306fa6b1086c18d76701ff4141a
OwenNeros <111894441+OwenNeros@users.noreply.github.com>
2022-08-26 10:59:31 -0500

    version 1

---
+++ HelloWorld/HelloWorld/My First Project/My First Project/main.cpp
@@ -0,0 +1,16 @@
+/*
+  Owen Neros
+  C++ Fall 2022
+  Due: August 31 2022
+  Lab 1 Introduction to C++ Programming
+  This lab demonstrates my skilld with IDE and GitHub
+ */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << " Blue";
+    
+    return 0;
+}
---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,21 @@
+//
+//  Owen Neros
+//  C++ Fall Semester 2022
+//  Due: August 31st
+//  Lab 1 Introduction to C++ Programming
+//  Demonstration of running and sharing first C++ program
+//
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Owen Neros on 8/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
